c29f33074650c36452116795af17d081f32e7b21
jarkynai-st <jarkynai15@mail.ru>
2021-01-05 18:02:07 +0600

    autorization

---
+++ work2.py
@@ -0,0 +1,18 @@
+username = "maksimka"
+password = '1,2,3,4,,5,6'
+
+def sign_in():
+    tries = 0
+    while tries < 3:
+
+        check_username = input()
+        check_password = input()
+        if check_username == username and check_password == password:
+            print('Вы успешно вошли!')
+            break
+        else:
+            print('Вы вели неверные данные!')
+
+        tries +=1
+
+sign_in()
