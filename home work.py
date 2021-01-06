b30f98b3b6a9b4703a93182ac9cbe7c2bf839ed3
jarkynai-st <jarkynai15@mail.ru>
2021-01-06 15:19:53 +0600

    home

--- home work.py
+++ home work.py
@@ -1,6 +1,28 @@
+# i = 0
+# while i ** 2 < 10:
+#     print(i**2)
+#     i+=1
+
+
+list1 = [1,2,3,4,5,]
 i = 0
-while i ** 2 < 10:
-    print(i**2)
-    i+=1
+result = 0
+while i < len(list1):
+      result = result + list1[i]
+      i = i + 1
+print(result)
+
+
+x = 9
+list1 = [1,2,3,4,5,6,7,8,9,9,9]
+i = 0
+count_9 = 0
+while i < len(list1):
+      if list1[i] == x:
+            count_9 += 1
+      i += 1
+print(count_9)
+
+
 
 
