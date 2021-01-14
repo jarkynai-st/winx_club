09e940e2c4bb7635195e431cb3669fcbbee1668c
jarkynai-st <jarkynai15@mail.ru>
2021-01-14 18:03:55 +0600

    count days

---
+++ week 3/day3/day2.py
@@ -0,0 +1,104 @@
+# list1 = [1,2,3,4,4,4,4,5,5]
+# list2 = []
+# for number in list1:
+#     if number not in list2:
+#          list2.append(number)
+#
+# # print(list2)
+#
+# for i in list1:
+#     print(list1.count(i))
+#
+# max_count = max(list2)
+# max_index = list2.index(max_count)
+# result = list1[max_index]
+# # print(result,max_count)
+#
+# list1 = [1,2,3,4,5]
+# dict1 = {}
+# for i in list1:
+#     dict1[i] = list1.count(i)
+# # print(dict1)
+#
+#
+# with open('Hello.txt','w') as f1:
+#     f1.write('hello')
+#     f1.write('123')
+#
+# w = int(input())
+# if 1<=w<=100:
+#     if w % 2 == 0:
+#         print('YES')
+#     else:
+#         print("NO")
+
+# str1 = input()
+# if len(str1) > 10:
+#     len_str = len(str1) - 2
+#     print(str1[0]+str(len_str)+str1[-1])
+# else:
+#     print(str1)
+
+
+# str1 = "list hello"
+# print(str1.split())
+
+
+# list1 = ['nurjanat','aigerim','jarkynai']
+# list1.remove(max(list1))
+
+# print(list1)
+
+
+# str1 = 'baaaaaaaaaaaaaaa'
+# a = 'a'
+# count_a = 0
+# for alpha in str1:
+#     if alpha == a:
+#         count_a += 1
+# if count_a == len(str1):
+    # print(True)
+# else:
+    # print(False)
+
+
+# str1 = "bishkek"
+# print(str1.upper())
+
+
+# str1 = "word1 word2 word3 word4"
+# z = str1.count(' ')
+# print(z+1)
+# print(len(str1.split()))
+
+
+# str1 = "WoRLddddddd"
+#
+# if str1[0].isupper() and len(str1) < 10:
+#     str1 = str1.lower()
+#     print(str1)
+# elif str1[0].islower() and len(str1) < 10:
+#     str1 = str1.upper()
+#     print(str1)
+# elif len(str1) > 10:
+#     str1 = str1.capitalize()
+    # print(str1)
+
+
+number = 20
+str1 = str(number)
+print(len(str1))
+
+
+
+str1 = "stroka"
+str1 = str1.replace('o','')
+
+
+
+
+
+
+
+
+
