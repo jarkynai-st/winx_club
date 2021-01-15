299af90b2f67c6e4861062fabc56ccead5459926
jarkynai-st <jarkynai15@mail.ru>
2021-01-15 16:29:53 +0600

    january

---
+++ week 3/work/home.py
@@ -0,0 +1,74 @@
+# str = 'green car'
+# print()
+# list1 = str.split()
+# # print(list1[0])
+#
+# str1 = 'aaaabbbb'
+# str1 = str1.replace('a','b')
+# print(str1)
+#
+# str = 'cccccdddd'
+# str = str.replace('c','d')
+# print(str)
+#
+# string = [5,7,8,9,10]
+# print(max(string))
+
+# string1 = '1 2 3 4 565 6 77 8 99'
+# string1 = string1.split()
+# list2 = []
+# for number in string1:
+#     list2.append(int(number))
+# print(max(list2))
+
+
+# def numbers(number):
+#     if len(number) == 13 and number.startswith("+996"):
+#         print(number)
+#     elif len(number) == 10 and number.startswith("0"):
+#         number = number.replace("0","+996")
+#         print(number)
+#     elif len(number) == 9:
+#         number = "+996" + number
+#         print(number)
+#     elif len(number) == 12 and number.startswith("996"):
+#         number = "+" + number
+#         print(number)
+#     else:
+#         print('NOT OK')
+#
+# numbers('+996777555444')
+#
+#
+# list1 = [1,2,3,4,5,6,7,8,9,9,9,9,9,99,9,9,555555,55]
+# list2 = [list1[0]],list1[2],list1[-2]
+# print(list2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
